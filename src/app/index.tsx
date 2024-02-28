ba483b5141f7e95872f5f0a542a803a4aabbb0e3
muhammadabdumalikov <burakali424@gmail.com>
2024-02-28 08:10:22 +0500

    add

---
+++ src/app/index.tsx
@@ -0,0 +1,47 @@
+import { Button, Form, Input, message, Space } from 'antd';
+import Image from "next/image";
+
+export default function FormComponent() {
+  const [form] = Form.useForm();
+
+  const onFinish = () => {
+    message.success('Submit success!');
+  };
+
+  const onFinishFailed = () => {
+    message.error('Submit failed!');
+  };
+
+  const onFill = () => {
+    form.setFieldsValue({
+      url: 'https://taobao.com/',
+    });
+  };
+
+
+  return <Form
+    form={form}
+    layout="vertical"
+    onFinish={onFinish}
+    onFinishFailed={onFinishFailed}
+    autoComplete="off"
+  >
+    <Form.Item
+      name="url"
+      label="URL"
+      rules={[{ required: true }, { type: 'url', warningOnly: true }, { type: 'string', min: 6 }]}
+    >
+      <Input placeholder="input placeholder" />
+    </Form.Item>
+    <Form.Item>
+      <Space>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+        <Button htmlType="button" onClick={onFill}>
+          Fill
+        </Button>
+      </Space>
+    </Form.Item>
+  </Form>
+}
