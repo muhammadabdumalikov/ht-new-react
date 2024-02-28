7f164921440a1d52e8a52acba4707621cfbe1218
muhammadabdumalikov <burakali424@gmail.com>
2024-02-28 09:40:01 +0500

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Avatar, Button, Card, Input, Image, Divider } from 'antd';
-import { MouseEventHandler, useState } from "react";
+import { Avatar, Button, Card, Input, Image, Divider, message } from 'antd';
+import { useState } from "react";
 
 import axios from 'axios';
 
@@ -16,6 +16,25 @@ export default function Home() {
 
   const { Meta } = Card;
 
+  const [messageApi, contextHolder] = message.useMessage();
+
+  // useEffect(() => {
+  //   if (userInfo.name.length > 0) {
+  //     // Trigger notice only if userInfo.name is available
+  //     messageApi.success({
+  //       content: 'User information loaded successfully',
+  //     });
+  //   }
+  // }, [userInfo.name, contextHolder, messageApi]);
+
+  const error = (err: any) => {
+    messageApi.open({
+      type: 'error',
+      content: err?.response?.data?.message,
+    });
+  };
+
+
   const onUrlInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUrl(e.target.value);
   };
@@ -23,7 +42,6 @@ export default function Home() {
   const onUrlInputSubmit = async () => {
     setUrlLoading(!urlLoading);
     axios.get('http://localhost:3000/profile', { params: { url } }).then(data => {
-      console.log(data);
       setUserInfo(prev => {
         return {
           ...prev,
@@ -34,24 +52,15 @@ export default function Home() {
         }
       })
       setUrlLoading(false);
+    }).catch(err => {
+      error(err);
+    }).finally(() => {
+      setUrlLoading(false);
     })
-    // setTimeout(() => setUrlLoading(false), 1000)
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24">
-
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          // priority
-        />
-      </div> */}
-
       <div className="flex w-[50%]">
         <Input placeholder="LinkedIn url" onChange={onUrlInputChange} />
         <Button type="primary" disabled={false} loading={urlLoading} onClick={onUrlInputSubmit}>Submit</Button>
@@ -71,6 +80,7 @@ export default function Home() {
           </Card>
           : null
         }
+        {contextHolder}
       </div>
     </main>
   );
