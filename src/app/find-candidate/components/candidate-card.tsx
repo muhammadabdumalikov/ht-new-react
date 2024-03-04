e10af27ddc84ae7bb8221a55428b8509aa481c39
muhammadabdumalikov <burakali424@gmail.com>
2024-03-04 17:53:38 +0500

    find-candidate

---
+++ src/app/find-candidate/components/candidate-card.tsx
@@ -0,0 +1,33 @@
+import { Button, Image } from "antd";
+import { EnvironmentOutlined, DollarOutlined, MessageOutlined, ArrowRightOutlined } from '@ant-design/icons';
+
+export default function CandidateCard() {
+  return <div className="flex items-center w-2/6 h-24 p-3 bg-white rounded-md">
+    <Image className="rounded-md" src="https://picsum.photos/70" />
+    
+    <div className="h-full w-[35%] ml-3">
+      <div className="h-[60%]">
+        <p className="text-black text-sm font-medium">Fresh Taylor</p>
+        <p className="text-black text-xs">Marketing Officer</p>
+      </div>
+
+      <div className="flex h-[40%]">
+        <span className="flex items-center mr-4">
+          <EnvironmentOutlined className="text-black mr-1"/>
+          <p className="text-black text-xs">New York</p>
+        </span>
+        <span className="flex items-center">
+          <DollarOutlined className="text-black mr-1"/>
+          <p className="text-black text-xs">3 years experiences</p>
+        </span>
+      </div>
+    </div>
+
+    <Button type="link" icon={<MessageOutlined className="text-black" />} />
+    
+    <div className="flex items-center justify-between w-40 h-8 bg-blue-600 p-2">
+      <p>View Profile</p>
+      <ArrowRightOutlined className="text-white"/>
+    </div>
+  </div>
+}
---
+++ src/app/find-candidate/page.tsx
@@ -0,0 +1,7 @@
+import CandidateCard from "./components/candidate-card";
+
+export default function FindCandidate() {
+  return <div className="w-full h-96 bg-red-400 p-5">
+    <CandidateCard/>
+  </div>
+}
